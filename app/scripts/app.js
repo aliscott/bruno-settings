f7724cfa20d0f58f33318dc561b7e549070c7106
jasonmelgoza <jason@rightscale.com>
2014-09-17 09:54:12 -0700

    make it so

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name brunoApp
+ * @description
+ * # brunoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('brunoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
